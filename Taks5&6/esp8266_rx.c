73a824ae266e045d913a133537a87e9e188841b1
KARthiKReddY563 <145685977+KARthiKReddY563@users.noreply.github.com>
2025-03-17 19:35:04 +0530

    Update esp8266_rx.c

--- Taks5&6/esp8266_rx.c
+++ Taks5&6/esp8266_rx.c
@@ -1,1 +1,39 @@
+unsigned long lastDebugTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("UART Receiver Started");
+}
+
+bool newLine = false;
+String receivedValue = "";
+void loop() {
+  // Check if data is available
+  while (Serial.available() > 0) {
+   while (Serial.available() > 0) {
+    char incomingByte = Serial.read();
+    
+    // If it's a newline character, process the complete number
+    if (incomingByte == '\n' || incomingByte == '\r' ) {
+      if (receivedValue.length() > 0 && !newLine) {
+        Serial.print("Distance: ");
+        Serial.print(receivedValue.toInt()); // Convert string to integer
+        Serial.println(" cm");
+        receivedValue = "";
+        newLine = true;
+        
+      }
+    } 
+    else {
+      
+        receivedValue += incomingByte;
+      newLine = false;
+      
+    }
+  }
+
+  
+  delay(100);
+}
+}
 
